c05d987781c84ffa883046b4d91810eb3a88601c
aleksey pavlov <alekseyelit1337@gmail.com>
2021-12-29 22:27:57 +0300

    Диалог с пользователем

--- FileEncryptor.WPF/Services/Interface/IUserDialog.cs
+++ FileEncryptor.WPF/Services/Interface/IUserDialog.cs
@@ -1,7 +1,10 @@
-﻿namespace FileEncryptor.WPF.Services.Interface
+﻿using System.Collections.Generic;
+
+namespace FileEncryptor.WPF.Services.Interface
 {
     internal interface IUserDialog
     {
-        
+        bool OpenFile(string Title, out string SelectedFIle, string Filter = "Все файлы (*.*)|*.*");
+        bool OpenFiles(string Title, out IEnumerable<string> SelectedFIles, string Filter = "Все файлы (*.*)|*.*");
     }
 }
--- FileEncryptor.WPF/Services/UserDialog.cs
+++ FileEncryptor.WPF/Services/UserDialog.cs
@@ -1,9 +1,49 @@
-﻿using FileEncryptor.WPF.Services.Interface;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FileEncryptor.WPF.Services.Interface;
+using Microsoft.Win32;
 
 namespace FileEncryptor.WPF.Services
 {
     internal class UserDialog : IUserDialog
     {
-        
+        public bool OpenFile(string Title, out string SelectedFIle, string Filter = "Все файлы (*.*)|*.*")
+        {
+            var file_dialog = new OpenFileDialog
+            {
+                Title = Title,
+                Filter = Filter
+            };
+
+            if (file_dialog.ShowDialog() != true)
+            {
+                SelectedFIle = null;
+                return false;
+            }
+
+            SelectedFIle = file_dialog.FileName;
+
+            return true;
+        }
+
+        public bool OpenFiles(string Title, out IEnumerable<string> SelectedFIles, string Filter = "Все файлы (*.*)|*.*")
+        {
+            var file_dialog = new OpenFileDialog
+            {
+                Title = Title,
+                Filter = Filter
+            };
+
+            if (file_dialog.ShowDialog() != true)
+            {
+                SelectedFIles = Enumerable.Empty<string>();
+                return false;
+            }
+
+            SelectedFIles = file_dialog.FileNames;
+
+            return true;
+
+        }
     }
 }
