8607cfb6e85f7e1e845d4c400c994854ed41671e
aleksey pavlov <alekseyelit1337@gmail.com>
2022-01-06 19:43:19 +0300

    Начало

---
+++ FileEncryptor.WPF/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FileEncryptor.WPF.Annotations;
+
+namespace FileEncryptor.WPF.ViewModels.Base
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
